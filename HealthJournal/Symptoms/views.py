35f8b39876f4d83dada7f98115e67173e2c60fa5
tahabornoosh <tmbornoosh@gmail.com>
2026-01-10 17:07:07 -0800

    Commit 7

--- HealthJournal/Symptoms/views.py
+++ HealthJournal/Symptoms/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import journal
+from django.contrib.auth import get_user_model
+from PersonalInfo.models import info
+from Nutrition.models import nutrition
 # Create your views here.
 def seeAll(request):
     if request.method=="GET":
@@ -11,4 +14,15 @@ def seeAll(request):
         return render(request, 'symptoms.html', {'journal':journal.objects.get(id=1)})
 
 def suggestions(request):
-    return render(request, "suggestions.html", {})
+    u = info.objects.get(id=1)
+    q = "Consider these information about this person and give them 5-10 new food suggestions addressing their diet and health concerns. Return your answer in plaintext only. Nutrition Preferences:"+str(u.nutrition_preferences)+" || Medical Conditions: "+str(u.medical_conditions)
+    objs = nutrition.objects.all()
+    for obj in objs:
+        q+=f"|| FOOD LOG: title:{obj.title}, type:{obj.food_drink}, date:{obj.datetime}, notes:{obj.notes}"
+    from google import genai
+    from django.conf import settings
+    g = genai.Client()
+    response = g.models.generate_content(
+        model="gemini-2.5-flash", contents=q
+    )
+    return render(request, "suggestions.html", {"r":response.text})
